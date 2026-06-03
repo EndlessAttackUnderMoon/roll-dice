fff3cf3f52c544b98951d68f14dbc95037301568
EndlessAttackUnderMoon <19212132919@163.com>
2026-06-03 14:16:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import random
+import astrbot.api.message_components as Comp
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+imageURL = "imageURL"
+number = ["1ImageName", "2ImageName", "3ImageName", "4ImageName", "5ImageName", "6ImageName"]
+
+@register("掷骰子", "EndlessAttackUnderMoon", "随机发出一张骰子的图片。", "1.1")
+class PluginRollDice(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+
+    @filter.command("掷骰子")
+    async def helloworld(self, event: AstrMessageEvent):
+        """这是一个AstrBot的 掷骰子 指令，可随机发出一张骰子的图片。""" # handler描述
+        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        logger.info(message_chain)
+
+        name = random.choice(number)
+        path = f"{imageURL}/{name}.png"
+
+        chain = [
+            Comp.Image.fromURL(path)
+        ]
+
+        yield event.chain_result(chain) # 发送一条富媒体消息
